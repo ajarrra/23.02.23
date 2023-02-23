659f766644985ef1bda0f9613cc41154ca2d02cd
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-23 13:14:09 +0100

    added and edited button

--- script.js
+++ script.js
@@ -15,6 +15,25 @@ console.log(example3);
 let example4 = document.querySelector("p:nth-child(4");
 example4.style.width = "200px";
 example4.style.height = "200px";
-example4.style.transform = "rotate(-45deg)"
+example4.style.transform = "rotate(-45deg)";
+example4.style.margin = "30px";
 
 console.log(example4)
+
+
+//события
+let helloBtn = document.querySelector("#hello");
+helloBtn.addEventListener("click", function() { //клик , кторая ждет
+  alert("hello!");//надпись которая выводит
+})
+
+//кнопку дать значение
+
+let byeBth = document.querySelector("#bye");
+byeBth.addEventListener("click", function(){
+    alert("Bye");
+});
+
+
+
+
