efa8390da9c5fe708b31c968aec3e2f6dee578ef
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-23 12:43:31 +0100

    edited example 4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let example1 = document.querySelector("p");
-example1.textContent = "🤗" +example1.textContent + "😋";
+example1.textContent = "🤗" + example1.textContent + "😋";
 console.log(example1);
 
 let example2 = document.querySelector(".example-class");
@@ -9,7 +9,12 @@ console.log(example2);
 
 
 let example3 = document.querySelector("#example-id");
+example3.style.backgroundImage = "url(photo.avif)" ;
 console.log(example3);
 
 let example4 = document.querySelector("p:nth-child(4");
+example4.style.width = "200px";
+example4.style.height = "200px";
+example4.style.transform = "rotate(-45deg)"
+
 console.log(example4)
