52374f6ee13327172ddb041dd1d2861664954367
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-23 12:25:23 +0100

    tried to added a smile for example 1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let example1 = document.querySelector("p");
-console.log(example1)
+example1.textContent +="🤗";
+console.log(example1);
 
+let example2 = document.querySelector(".example-class");
+console.log(example2);
+
+
+let example3 = document.querySelector("#example-id");
+console.log(example3);
+
+let example4 = document.querySelector("p:nth-child(4");
+console.log(example4)
